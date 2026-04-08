953e0b8faa14ac164b3249af2e96fedc03de96bc
Karen <957458041@qq.com>
2026-04-08 22:19:53 +0800

    feat: update nav

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Notes', link: '/ios/Swift 内存' }
+      { text: '主页', link: '/' },
+      { text: '笔记', link: '/ios/Swift 内存' }
     ],
 
     sidebar: [
